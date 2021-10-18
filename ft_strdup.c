4a106df3e62380314944e893cc565e0dd8cdb619
Semih Beyazkilic <beyazkilic.semih@gmail.com>
2021-10-19 00:23:45 +0200

    update

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: seozcan <seozcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 12:22:30 by seozcan           #+#    #+#             */
-/*   Updated: 2021/10/18 13:03:23 by seozcan          ###   ########.fr       */
+/*   Updated: 2021/10/19 00:21:44 by seozcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strdup(const char *s1)
 {
 	const char	*s2;
 
+	if (!s1)
+		return (NULL);
 	s2 = (char *)malloc(sizeof(char) * sizeof(s1));
 	s2 = s1;
 	return ((char *)s2);
